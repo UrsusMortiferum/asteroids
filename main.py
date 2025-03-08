fe7b08de81e80633c7c6e4c6dfe6f82a63e603e4
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-03-08 23:38:45 +0100

    fps?

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    print(f"""
+Starting Asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}
+          """)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-    print(f"""
-Starting Asteroids!
-Screen width: {SCREEN_WIDTH}
-Screen height: {SCREEN_HEIGHT}
-          """)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
