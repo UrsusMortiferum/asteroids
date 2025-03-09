e4e989f11c0b5b0229a024a854da19885e411f3d
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-03-09 14:16:22 +0100

    player object!

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     print(f"""
 Starting Asteroids!
@@ -18,6 +22,7 @@ Screen height: {SCREEN_HEIGHT}
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
