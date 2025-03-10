73b34e469a81e5eef6f2572011533d2390ee82a1
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-03-10 11:34:44 +0100

    fixed rotation when moving back

--- player.py
+++ player.py
@@ -18,21 +18,24 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), width=2)
         
-    def rotate(self, dt):
-        self.rotation += PLAYER_ROTATION_SPEED * dt
+    def rotate(self, dt, direction):
+        self.rotation += PLAYER_ROTATION_SPEED * dt * direction
+
+    def move(self, dt, direction):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_MOVE_SPEED * dt * direction
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        direction = 1
+        if keys[pygame.K_s]:
+            direction = -1
 
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.move(dt, direction)
         if keys[pygame.K_s]:
-            self.move(-dt)
+            self.move(dt, direction)
         if keys[pygame.K_a]:
-            self.rotate(-dt)
+            self.rotate(-dt, direction)
         if keys[pygame.K_d]:
-            self.rotate(dt)
-
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_MOVE_SPEED * dt
+            self.rotate(dt, direction)
