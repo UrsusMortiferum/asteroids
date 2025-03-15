db52990f08a0b2e68ef1edf44900f6488d40a0a9
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-03-15 23:45:50 +0100

    Added asteroids

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        color = (255, 78, 10)
+        pygame.draw.circle(screen, color, self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
